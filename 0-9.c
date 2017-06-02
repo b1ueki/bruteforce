88638e45f8af51853b77e3eeaaacf5c7cd126267
岩田和俊 <k4z@iwatakazutoshinoMacBook-Pro.local>
2017-06-02 10:11:40 +0900

    upgrade
    divided for each function

---
+++ 0-9.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+void bruteImpl(char*, int, int);
+void bruteSequential(int);
+
+static const char alphabet[] = "0123456789";
+static const int alphabetSize = sizeof(alphabet) - 1;
+char pass[];
+
+int main()
+{
+  int pass_length;
+
+  printf("please input pass_length >");
+  scanf("%d", &pass_length);
+  printf("please input password(up to %d characters) >", pass_length);
+  scanf("%s",pass);
+  bruteSequential(pass_length);
+  return 0;
+}
+
+void bruteImpl(char* str, int index, int maxDepth)
+{
+  static int j = 0;
+  for (int i = 0; i < alphabetSize; ++i)
+  {
+    str[index] = alphabet[i];
+
+    if (index == maxDepth - 1){
+      printf("%s  ", str);
+      if(j++ % 30 == 0)
+        printf("\n");
+      if(!strcmp(str, pass)){
+        printf("\n----------------\n");
+        printf("pass is [%s]\n",str);
+        printf("----------------\n");
+        exit(1);
+      }
+    }
+    else bruteImpl(str, index + 1, maxDepth);
+  }
+}
+
+void bruteSequential(int maxLen)
+{
+  char* buf = malloc(maxLen + 1);
+
+  for (int i = 1; i <= maxLen; ++i)
+  {
+    memset(buf, 0, maxLen + 1);
+    bruteImpl(buf, 0, i);
+  }
+
+  free(buf);
+}
+
--- bruteforce.c
+++ bruteforce.c
@@ -1,53 +1,16 @@
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
+int main(){
+  int mode;
 
-#define PASS_LENGTH 4
+  printf("please select bruteforce mode\n");
+  printf("1:all  2:a-z  3:0-9 >");
+  scanf("%d", &mode);
 
-void bruteImpl(char*, int, int);
-void bruteSequential(int);
+  if(mode == 1)
+    system("./all");
+  else if(mode == 2)
+    system("./a-z");
+  else if(mode == 3)
+    system("./0-9");
 
-static const char alphabet[] = "abcdefghijklmnopqrstuvwxyz"
-                               "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-                               "0123456789";
-static const int alphabetSize = sizeof(alphabet) - 1;
-char pass[];
-
-int main(void)
-{
-  printf("please input password(up to 4 characters) >");
-  scanf("%s",pass);
-  bruteSequential(PASS_LENGTH);
-  return 0;
+    return 0;
 }
-
-void bruteImpl(char* str, int index, int maxDepth)
-{
-  for (int i = 0; i < alphabetSize; ++i)
-  {
-    str[index] = alphabet[i];
-
-    if (index == maxDepth - 1){
-      printf("%s\n", str);
-      if(!strcmp(str, pass)){
-        printf("password is [%s]\n",str);
-        exit(1);
-      }
-    }
-    else bruteImpl(str, index + 1, maxDepth);
-  }
-}
-
-void bruteSequential(int maxLen)
-{
-  char* buf = malloc(maxLen + 1);
-
-  for (int i = 1; i <= maxLen; ++i)
-  {
-    memset(buf, 0, maxLen + 1);
-    bruteImpl(buf, 0, i);
-  }
-
-  free(buf);
-}
-
